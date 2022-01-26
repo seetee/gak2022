4e8ff0fc250118d0b6d122faf80f255fb566332e
Kenneth Frantzen <github@frantzen.se>
2022-01-26 18:59:38 +0100

    Added D9 Barrio subtheme

---
+++ js/global.js
@@ -0,0 +1,16 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function ($, Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.gak2022 = {
+    attach: function (context, settings) {
+
+    }
+  };
+
+})(jQuery, Drupal);
